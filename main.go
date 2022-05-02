7cbec927fd977e8f0caaecc5039161da85d371df
ilyaKrivitskiy <krivitskiy-ilya@mail.ru>
2022-05-03 00:46:35 +0300

    string converted to weird case

--- main.go
+++ main.go
@@ -2,65 +2,31 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
-func validateLetter(a string) int {
-	switch string(a) {
-	case "I":
-		return 1
-	case "V":
-		return 5
-	case "X":
-		return 10
-	case "L":
-		return 50
-	case "C":
-		return 100
-	case "D":
-		return 500
-	case "M":
-		return 1000
-	default:
-		return 0
-	}
-}
+func toWeirdCase(str string) string {
+	res := ""
+	arr := strings.Split(str, " ")
 
-func canBeDiff(a, b string) bool {
-	if (a == "I" && b == "V") || (a == "I" && b == "X") || (a == "X" && b == "L") || (a == "X" && b == "C") ||
-		(a == "C" && b == "D") || (a == "C" && b == "M") {
-		return true
-	} else {
-		return false
-	}
-
-}
-
-func Decode(roman string) int {
-	strlen := len(roman)
-
-	cur := validateLetter(string(roman[0]))
-
-	if strlen == 1 {
-		return cur
-	}
-
-	for i := 0; i < strlen-1; i++ {
-		a, b := validateLetter(string(roman[i])), validateLetter(string(roman[i+1]))
-
-		if a >= b {
-			cur += b
-		} else {
-			if canBeDiff(string(roman[i]), string(roman[i+1])) {
-				cur = b - 2*a + cur
+	for j, s := range arr {
+		for i := 0; i < len(s); i++ {
+			if i%2 == 0 {
+				res += strings.ToUpper(string(s[i]))
 			} else {
-				cur += b
+				res += strings.ToLower(string(s[i]))
 			}
+		}
+		if j != len(arr)-1 {
+			res += " "
 		}
 	}
 
-	return cur
+	return res
 }
 
 func main() {
-	fmt.Println(Decode("M"))
+	if strings.Compare(toWeirdCase("This is a test Looks like you passed"), "ThIs Is A TeSt LoOkS LiKe YoU PaSsEd") == 0 {
+		fmt.Println("True")
+	}
 }
