1da62b46b947f6a75041a62ba0bb548e63b19ac0
ilyaKrivitskiy <krivitskiy-ilya@mail.ru>
2022-05-03 21:31:51 +0300

    Tortoise racing

--- main.go
+++ main.go
@@ -2,31 +2,28 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func toWeirdCase(str string) string {
-	res := ""
-	arr := strings.Split(str, " ")
+func Race(v1, v2, g int) [3]int {
+	res := [3]int{}
 
-	for j, s := range arr {
-		for i := 0; i < len(s); i++ {
-			if i%2 == 0 {
-				res += strings.ToUpper(string(s[i]))
-			} else {
-				res += strings.ToLower(string(s[i]))
-			}
-		}
-		if j != len(arr)-1 {
-			res += " "
-		}
+	if v1 > v2 {
+		return [3]int{-1, -1, -1}
 	}
+
+	t := float32(g) / (float32(v2) - float32(v1))
+	res[0] = g / (v2 - v1)
+	t -= float32(res[0])
+	t *= 60
+	min := int(t)
+	res[1] = min
+	t -= float32(min)
+	res[2] = int(t * 60)
 
 	return res
 }
 
 func main() {
-	if strings.Compare(toWeirdCase("This is a test Looks like you passed"), "ThIs Is A TeSt LoOkS LiKe YoU PaSsEd") == 0 {
-		fmt.Println("True")
-	}
+	fmt.Println(Race(820, 81, 550))
+
 }
