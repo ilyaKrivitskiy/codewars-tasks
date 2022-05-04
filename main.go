308476fb1855cab5e9356c2cbd77eddb43b1a754
ilyaKrivitskiy <krivitskiy-ilya@mail.ru>
2022-05-05 01:08:30 +0300

    Moving Zeros To The End

--- main.go
+++ main.go
@@ -1,30 +1,31 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
 
-func CreatePhoneNumber(numbers [10]uint) string {
-	res := ""
+func MoveZeros(arr []int) []int {
+	c := 0
 
-	res += "(" +
-		strconv.FormatUint(uint64(numbers[0]), 10) +
-		strconv.FormatUint(uint64(numbers[1]), 10) +
-		strconv.FormatUint(uint64(numbers[2]), 10) +
-		") " +
-		strconv.FormatUint(uint64(numbers[3]), 10) +
-		strconv.FormatUint(uint64(numbers[4]), 10) +
-		strconv.FormatUint(uint64(numbers[5]), 10) +
-		"-" +
-		strconv.FormatUint(uint64(numbers[6]), 10) +
-		strconv.FormatUint(uint64(numbers[7]), 10) +
-		strconv.FormatUint(uint64(numbers[8]), 10) +
-		strconv.FormatUint(uint64(numbers[9]), 10)
+	mas := make([]int, 0, cap(arr))
 
-	return res
+	for i, x := range arr {
+		if x == 0 {
+			c++
+			continue
+		}
+		mas = append(mas, arr[i])
+	}
+	if c != 0 {
+		for i := 0; i < c; i++ {
+			mas = append(mas, 0)
+		}
+	}
+
+	return mas
 }
 
 func main() {
-	fmt.Println(CreatePhoneNumber([10]uint{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}))
+	arr0 := []int{1, 2, 0, 1, 0, 1, 0, 3, 0, 1}
+	arr := []int{5, 0, 6, 7, 8, 1, 1, 3, 1, 9, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0}
+	fmt.Println(MoveZeros(arr0))
+	fmt.Println(MoveZeros(arr))
 }
